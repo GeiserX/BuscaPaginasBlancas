73f03e1c91e2bd389faf063879e803956cd7fc78
GeiserX <9169332+GeiserX@users.noreply.github.com>
2017-09-04 00:40:17 +0200

    First commit

---
+++ crawler.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+def PyCrawler(apellido):
+
+    import sqlite3
+
+    bd = sqlite3.connect('paginasblancas.db') #bd = sqlite3.connect(:memory:)
+
+    cursor = bd.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS paginasblancas
+                      (Nombre text, Apellido1 text, Apellido2 text, Telefono text PRIMARY KEY, Calle text, CP text, CiudadRegion text) 
+                   """)
+
+    pa = apellido1(apellido)
+    getInfo(pa, cursor, bd)
+
+    pa = apellido2(apellido)
+    getInfo(pa, cursor, bd)
+
+    ## Cambiar el apellido y también hacerlo con la A ##
+    apellidoMujer = "".join(apellido + "a")
+
+    pa = apellido1(apellidoMujer)
+    getInfo(pa, cursor, bd)
+
+    pa = apellido2(apellidoMujer)
+    getInfo(pa, cursor, bd)
+
+    cursor.close()
+
+    print("Finished!")
+
+
+def apellido1(apellido):
+
+    import requests
+    pa = requests.get('http://blancas.paginasamarillas.es/jsp/resultados.jsp?ap1=%s&sec=30&pgpv=1&tbus=0&nomprov=Murcia&idioma=spa' % apellido)
+    return pa
+
+
+def apellido2(apellido):
+
+    import requests
+    pa = requests.get('http://blancas.paginasamarillas.es/jsp/resultados.jsp?ap2=%s&sec=30&pgpv=1&tbus=0&nomprov=Murcia&idioma=spa' % apellido)
+    return pa
+
+
+def getInfo(pa,cursor,bd):
+
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(pa.content, 'html.parser')
+
+    # ## Si se quisiera seguir mirando en las siguientes páginas... ##
+    # searchNumber = int(soup.find_all('strong')[11].contents[0])
+    #
+    # for x in range(math.ceil(searchNumber/10)):
+    # ##
+
+    ### BUSCAR NOMBRES ###
+    h3 = soup.find_all('h3')
+
+    names = []
+    for item in h3:
+        names.append(item.contents[-1].strip().replace(u'\xa0\xa0', u' '))
+        #print(list(item.children)[1].get_text())
+
+    ### BUSCAR TELÉFONOS ###
+    telef = soup.find_all('span', class_='telef')
+
+    telefonos=[]
+    for item in telef:
+        telefonos.append(item.contents[2][-11:].replace(u' ', u'')) if item.contents[2][-11].isdigit() else "" ## Comprobar si no es un "Otros teléfonos"
+
+    ### BUSCAR DIRECCIÓN COMPLETA ###
+    calle = []
+    cp = []
+    lugar = []
+    for item in h3:
+        p = item.find_next_sibling('p')
+        calle.append(str(p.contents).split("\\t")[1].replace(u"\\r\\n", "").replace(u" ", u""))
+        cp.append(str(p.contents).split("\t")[5].split("-")[0].replace(u"\xa0", u""))
+        lugar.append(",".join(str(p.contents).split("\t")[5].split("-")[1:]).replace(u"\xa0", u"").replace(u"\r\n",u"").replace(u" ", u""))
+
+    for i in range(len(h3)):
+        sql="INSERT OR IGNORE INTO paginasblancas VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')" % ("".join(names[i].split()[:-2]), names[i].split()[-2], names[i].split()[-1], telefonos[i], calle[i], cp[i], lugar[i])
+        cursor.execute(sql)
+
+    bd.commit()
